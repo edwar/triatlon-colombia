0bc33ef8395d8a14235430ca15d00f10e5faa040
Edwar Amaya Diaz <edwaramayadiaz@gmail.com>
2023-02-23 11:16:56 -0500

    Not close nav in mobile

--- src/hooks/useNav.ts
+++ src/hooks/useNav.ts
@@ -1,19 +1,10 @@
 import { usePathname } from 'next/navigation'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { Navigator } from '@/types'
 
 export function useNav (nav: Navigator[]) {
   const [active, setActive] = useState(false)
   const pathname = usePathname()
-
-  useEffect(() => {
-    if (!active) return
-    document.addEventListener('click', handleToggle)
-
-    return () => {
-      document.removeEventListener('click', handleToggle)
-    }
-  })
 
   nav.forEach(item => {
     item.isActive = item.path === pathname
